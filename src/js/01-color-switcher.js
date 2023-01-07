bebe3f97b3a58d2dea5e56d2ea21a5f88d41e487
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-01-07 16:54:43 +0200

    first-commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+let intervalId = null;
 
+console.log(startBtn);
+
+startBtn.addEventListener(`click`, onStartBtnClick);
+stopBtn.addEventListener(`click`, onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+    intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
