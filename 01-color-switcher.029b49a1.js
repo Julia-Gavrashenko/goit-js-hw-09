36ec4d7b010a1cab207bed9bd00092748d8349b3
Julia-Gavrashenko <110378938+Julia-Gavrashenko@users.noreply.github.com>
2023-01-08 15:56:52 +0000

    Deploying to gh-pages from @ Julia-Gavrashenko/goit-js-hw-09@bb176823d284a8e11ccace2075a202b72d200f43 🚀

---
+++ 01-color-switcher.029b49a1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;console.dir(t),console.dir(e),t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.029b49a1.js.map
