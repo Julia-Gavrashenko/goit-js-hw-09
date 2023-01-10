0cfdc748013dbbeab10e180f47e59d76faf5adc4
Julia-Gavrashenko <110378938+Julia-Gavrashenko@users.noreply.github.com>
2023-01-10 16:56:23 +0000

    Deploying to gh-pages from @ Julia-Gavrashenko/goit-js-hw-09@c6bb35862f33f333bc16a09f56b5ae733939f4fa 🚀

--- 03-promises.0410d9d7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");e.addEventListener("submit",(function(e){e.preventDefault();var t=e.target.elements,n=t.delay.value,l=t.step.value,o=t.amount.value;console.log(n,l,o)})),console.log(e.elements)}();
-//# sourceMappingURL=03-promises.0410d9d7.js.map
--- 03-promises.737cee00.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");e.addEventListener("submit",(function(e){e.preventDefault();const{delay:{value:t},step:{value:n},amount:{value:o}}=e.target.elements;console.log(t,n,o)})),console.log(e.elements);
-//# sourceMappingURL=03-promises.737cee00.js.map
