0672b82bca3374c351e3cd004bf77de3837d7584
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-01-10 11:50:08 +0200

    seventh-commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     if (shouldResolve) {
+//       // Fulfill
+//     } else {
+//       // Reject
+//     }
+//   });
+// }
+
+const form = document.querySelector(`.form`);
+form.addEventListener(`submit`, onFormSubmit);
+console.log(form.elements);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    delay: { value: delay },
+    step: { value: step },
+    amount: { value: amount },
+  } = event.target.elements;
+
+  console.log(delay, step, amount);
 }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(() => {
+//       resolve({ position, delay });
+//     }, delay);
+//   });
+// }
+
+// createPromise(position, delay)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
